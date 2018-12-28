173b2ffbc4e793c67cc4e2b819e7eae9c73dd2b4
Khaliq Ansari <khaliqansari771@gmail.com>
2018-12-28 11:06:04 -0800

    Add files via upload

---
+++ avg.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter Number 1 "))
+num2 = int(input("Enter Number 2 "))
+num3 = int(input("Enter Number 3 "))
+total = int(num1+num2+num3)/3
+print(f"your avg of {num1} and {num2} and {num3} is {total} ")
